997cbfb97860198fe07de8f738ef8375eb2e836e
Ivan Kvyatkovskiy <ivan.kvyatkovski@gmail.com>
2022-12-05 13:20:05 +0700

    adds xcode project

---
+++ stable-diffusion-sample/ContentView.swift
@@ -0,0 +1,108 @@
+//
+//  ContentView.swift
+//  stable-diffusion-sample
+//
+//  Created by Ivan Kvyatkovskiy on 05/12/2022.
+//
+
+import SwiftUI
+
+// input state
+// timer for generation
+// copy to pasteboard
+
+struct ContentView: View {
+    @ObservedObject var model = Model()
+    @State var input: String = "a photo of an astronaut riding a horse on mars"
+
+    @State var currentDate = Date.now
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+
+    var body: some View {
+        VStack {
+            switch model.image {
+                case .generated(let image, let input, let time):
+                    HStack {
+                        Image(image,
+                              scale: 1,
+                              label: Text("LABEL"))
+                        Text("\(input)").font(.italic(.body)())
+                    }
+
+                    Text("Generated in \(time) seconds")
+                    Button("Again") {
+                        model.reset()
+                    }
+                case .generating(let at, let input):
+                    VStack(spacing: 10) {
+                        Text("\(input)").font(.italic(.body)())
+                        ProgressView()
+                        let distance = max(Int(at.distance(to: currentDate)), 0)
+                        Text("\(distance)").monospacedDigit()
+                    }.onReceive(timer) { now in
+                        currentDate = now
+                    }
+
+                case .input:
+                    VStack {
+                        TextField("enter text",
+                                  text: $input,
+                                  axis: .vertical)
+                        .lineLimit(nil)
+                        .textFieldStyle(.roundedBorder)
+                        Button("Generate") {
+                            model.generate(input)
+                        }
+                    }
+                case .error(let error):
+                    Text(error.localizedDescription)
+                    Button("Again") {
+                        model.reset()
+                    }
+            }
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+import StableDiffusion
+
+enum ModelState {
+    case generated(image: CGImage, input: String, time: Int)
+    case generating(startedAt: Date, input: String)
+    case input
+    case error(Error)
+}
+
+class Model: ObservableObject {
+    @Published var image: ModelState = .input
+    let q = DispatchQueue.global(qos: .userInitiated)
+
+    func generate(_ input: String) {
+        let startedAt = Date()
+        image = .generating(startedAt: startedAt, input: input)
+        let resourceURL = Bundle.main.resourceURL!
+        q.async {
+            do {
+                let pipeline = try StableDiffusionPipeline(resourcesAt: resourceURL)
+                let seed = 93 //Int.random(in: Int.min...Int.max)
+                let cgImage = try pipeline.generateImages(prompt: input, seed: seed).first!!
+                DispatchQueue.main.async {
+                    self.image = .generated(image: cgImage, input: input, time: Int(startedAt.distance(to: Date())))
+                }
+            } catch {
+                self.image = .error(error)
+            }
+        }
+    }
+
+    func reset() {
+        image = .input
+    }
+}
---
+++ stable-diffusion-sampleUITests/stable_diffusion_sampleUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  stable_diffusion_sampleUITestsLaunchTests.swift
+//  stable-diffusion-sampleUITests
+//
+//  Created by Ivan Kvyatkovskiy on 05/12/2022.
+//
+
+import XCTest
+
+final class stable_diffusion_sampleUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
